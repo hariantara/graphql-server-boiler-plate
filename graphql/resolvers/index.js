ebc4bbf88e528bc492b77d6a17f515c16e49385a
hari antara <hariantara.iputu@gmail.com>
2019-01-21 10:09:58 +0700

    update resolvers root

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,15 @@
 //Query
-// will add here . . .
+const notifications = require('./queries/notification')
+// add your queries here like <notifications>
 
 //Mutation
-
 // Notification 
-const notifications = require('./queries/notification')
 const pushNotification = require('./mutations/Push_Notification/push_notification')
+// add your queries here like <pushNotification>
+
+// Subcription
 const newNotifications = require('./subscription/new_notification')
+// add your queries here like <newNotifications>
 
 const resolversFunc = {
     notifications,
